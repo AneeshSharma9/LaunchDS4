b1a658e3d7b150458178d3c69d3c690abd090ce9
AneeshSharma9 <ansh993@gmail.com>
2022-08-03 00:09:54 -0400

    initial commit

---
+++ LaunchDS4/LaunchDS4.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+
+os.system('net stop NvContainerLocalSystem')
+
+#stopstr = 'stop-service "NvContainerLocalSystem"'
+#subprocess.run(['powershell', stopstr], shell=True)
+
+os.system("C:\\Users\\ansh9\\Downloads\\DS4Windows_3.1.1_x64\\DS4Windows\\DS4Windows.exe")
