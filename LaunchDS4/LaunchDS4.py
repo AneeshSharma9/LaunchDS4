099a25410ac488457c273545f1562d50c22969ff
Aneesh Sharma <101680397+AneeshSharma9@users.noreply.github.com>
2023-07-08 11:31:18 -0400

    Update LaunchDS4.py

--- LaunchDS4/LaunchDS4.py
+++ LaunchDS4/LaunchDS4.py
@@ -3,7 +3,5 @@ import subprocess
 
 os.system('net stop NvContainerLocalSystem')
 
-#stopstr = 'stop-service "NvContainerLocalSystem"'
-#subprocess.run(['powershell', stopstr], shell=True)
-
+#REPLACE YOUR FILE PATH TO DS4 HERE
 os.system("C:\\Users\\ansh9\\Downloads\\DS4Windows_3.1.1_x64\\DS4Windows\\DS4Windows.exe")
